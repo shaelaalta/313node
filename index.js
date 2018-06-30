f3c78956b84c3dade03455638ba4087fa5c438a0
Shaela <sut16006@byui.edu>
2018-06-29 22:13:59 -0600

    messin with form.18

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function getPerson(request, response){
 }
 
 function getFamilyInfo(lname, mom, dad, city, state, street, password, callback){
-    var sql = "INSERT INTO family VALUES (DEFAULT, '$3', '$2', '$1', '$6', '$4', '$5', '$7')";
+    var sql = "INSERT INTO family VALUES (DEFAULT, $3, $2, $1, $6, $4, $5, $7)";
     var params = [lname, mom, dad, city, state, street, password];
     pool.query(sql, params, function(err, result){
         if(err){
