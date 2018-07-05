e98ea40f160b56fddcbf22176569db0a85a1d0e7
Shaela <sut16006@byui.edu>
2018-07-05 10:28:29 -0600

    link to add member.2

--- public/main.js
+++ public/main.js
@@ -56,7 +56,7 @@ function showPpl(ppl){
     var show = "";
     var i;
     var len = list.length;  
-    show += "<form action='/addMem' method='POST'><input type='hidden' name='famId' value='" + list[0].famid + "'><input type='submit' value='Add Family Member'></form>";
+    show += "<form action='/addMemPage' method='POST'><input type='hidden' name='famId' value='" + list[0].famid + "'><input type='submit' value='Add Family Member'></form>";
     for(i = 0; i < len; i++){
         show += "<div id='secFam'>";
         show += "<h2>Name: "+ list[i].firstname + "</h2>";
