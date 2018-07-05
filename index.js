d8fd58de79fab06d612f2cccef2deb2cb94d9f41
Shaela <sut16006@byui.edu>
2018-07-05 11:17:14 -0600

    upload images.2

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function addFile(req, res){
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files){
         var oldpath = files.filetoupload.path;
-        var newpath = 'images/' + files.filetoupload.name;
+        var newpath = '/images/' + files.filetoupload.name;
         fs.rename(oldpath, newpath, function(err){
             if (err) throw err;
             console.log("files uploaded and moved");
