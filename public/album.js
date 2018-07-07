82445ec23f0da066fac921043478b135cb9c3bb6
Shaela <sut16006@byui.edu>
2018-07-06 22:51:41 -0600

    see albums aaa.1

--- public/album.js
+++ public/album.js
@@ -2,6 +2,7 @@ function getAlbums(){
     xhttp = new XMLHttpRequest();
     var famId = document.getElementById('famId').textContent;
     var id = document.getElementById('id').textContent;
+    console.log("album page: " + famId);
     var url = '/albumBunch?fid=' + famId + '&id=' + id;
     xhttp.open("GET", url, true);
     xhttp.onreadystatechange = function(){
@@ -13,7 +14,18 @@ function getAlbums(){
 }
 
 function showAlbums(data){
-    
+    var happy = data.response;
+    happy = JSON.parse(happy);
+    var list = happy.pic;
+    var show = "";
+    var i;
+    var len = list.length;
+    for(i = 0; i < len; i++){
+        show += "<div id='secFam'>";
+        show += "<h2>Name: "+ list[i].albumname + "</h2>";
+        show += "</div>";
+    }
+    document.getElementById("albums").innerHTML = show;
 }
 
 window.addEventListener('load', getAlbums);
