3542aef3f78b9c134fbd21bf52622cee713897ba
Shaela <sut16006@byui.edu>
2018-07-12 15:20:56 -0600

    session test..1

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ express()
     addJournal(req, res);
 })
 
-.post('/famLogin', urlencodedParser, function(req, res){
+/*.post('/famLogin', urlencodedParser, function(req, res){
     var result = {success: false};
     
     console.log("index.js " + req.body.username + " and password "+ req.body.password);
@@ -158,7 +158,9 @@ express()
     }
     
     res.json(result);
-})
+})*/
+
+.post('/famLogin', urlencodedParser, handleLogin)
 
 .post('/fileupload', upload.single('image'), urlencodedParser, function(request, response){
     cloudinary.uploader.upload(request.file.path, function(result){
@@ -177,6 +179,19 @@ express()
 })
 
 .listen(PORT, () => console.log(`listening on port ${ PORT }`));
+
+function handleLogin() {
+    var result = {success: false};
+    
+    console.log("index.js " + req.body.username + " and password "+ req.body.password);
+    
+    if(req.body.username == "happy" && req.body.password == "day"){
+        //req.session.user == req.body.username;
+        result = {success: true};
+    }
+    
+    res.json(result);
+}
 
 /***************************************
 * add image to image db
