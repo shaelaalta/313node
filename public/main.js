d00f6145c69b30470abb6127c655e007d632b294
Sutton-Shaela <sut16006@byui.edu>
2020-01-23 11:32:40 -0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -11,8 +11,7 @@ function showAllFams(){
 }
 
 function loadData(items){
-    console.log("here");
-    console.log(items.response);
+    //console.log(items.response);
     var thing = items.response;
     thing = JSON.parse(thing);
     var list = thing.list;
